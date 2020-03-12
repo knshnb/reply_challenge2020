ac0c4e1641d314825420842de245a17940590ef6
Kenshin Abe <abe.kenshin@gmail.com>
2020-03-13 02:34:31 +0900

    Simulator (knshnb)

--- main.cpp
+++ main.cpp
@@ -8,6 +8,12 @@ struct SetupIO { SetupIO() { cin.tie(nullptr), ios::sync_with_stdio(false), cout
 #define dump(...)
 #endif  // clang-format on
 
+const vector<pair<int, int>> DIRECTIONS = {
+    {1, 0}, {0, 1},  {-1, 0},  {0, -1},  // 4方向
+    {1, 1}, {-1, 1}, {-1, -1}, {1, -1},  // 斜め
+    {0, 0},                              // 自身
+};
+
 constexpr int MAX_SKILL_COUNT = 400;
 
 struct Developer {
@@ -30,26 +36,26 @@ vector<Manager> managers;
 // -3のとき、使えない
 // 0 <= i < nのとき、i番目のDeveloper
 // n <= i < n + mのときi - n番目のManager
-vector<vector<int>> pos;
+vector<vector<int>> ans_pos;
 
 void initialize_by_valid() {
     vector<pair<int, int>> valid_developer_pos, valid_manager_pos;
     for (int i = 0; i < H; i++) {
         for (int j = 0; j < W; j++) {
-            if (pos[i][j] == -1) valid_developer_pos.emplace_back(i, j);
-            if (pos[i][j] == -2) valid_manager_pos.emplace_back(i, j);
+            if (ans_pos[i][j] == -1) valid_developer_pos.emplace_back(i, j);
+            if (ans_pos[i][j] == -2) valid_manager_pos.emplace_back(i, j);
         }
     }
     for (int i = 0; i < num_developer; i++) {
         if (i < valid_developer_pos.size()) {
             auto [a, b] = valid_developer_pos[i];
-            pos[a][b] = i;
+            ans_pos[a][b] = i;
         }
     }
     for (int i = 0; i < num_manager; i++) {
         if (i < valid_manager_pos.size()) {
             auto [a, b] = valid_manager_pos[i];
-            pos[a][b] = i + num_developer;
+            ans_pos[a][b] = i + num_developer;
         }
     }
 }
@@ -58,11 +64,11 @@ void print_solution() {
     vector<pair<int, int>> developer_pos(num_developer, {-1, -1}), manager_pos(num_manager, {-1, -1});
     for (int i = 0; i < H; i++) {
         for (int j = 0; j < W; j++) {
-            if (0 <= pos[i][j]) {
-                if (pos[i][j] < num_developer) {
-                    developer_pos[pos[i][j]] = {i, j};
+            if (0 <= ans_pos[i][j]) {
+                if (ans_pos[i][j] < num_developer) {
+                    developer_pos[ans_pos[i][j]] = {i, j};
                 } else {
-                    manager_pos[pos[i][j] - num_developer] = {i, j};
+                    manager_pos[ans_pos[i][j] - num_developer] = {i, j};
                 }
             }
         }
@@ -97,21 +103,70 @@ inline int bonus_potential(const Developer& a, const Manager& b) {
     return a.company == b.company ? a.bonus * b.bonus : 0;
 }
 
+inline int bonus_potential(const Manager& a, const Manager& b) {
+    return a.company == b.company ? a.bonus * b.bonus : 0;
+}
+
+int total_potential(const vector<vector<int>>& pos, int i1, int j1, int i2, int j2) {
+    if (pos[i1][j1] < 0 || pos[i2][j2] < 0) return 0;
+    int ret = 0;
+    if (pos[i1][j1] < num_developer && pos[i2][j2] < num_developer) {
+        int dev1 = pos[i1][j1], dev2 = pos[i2][j2];
+        ret += work_potential(developers[dev1], developers[dev2]);
+        if (developers[dev1].company == developers[dev2].company) {
+            ret += bonus_potential(developers[dev1], developers[dev2]);
+        }
+    } else if (pos[i1][j1] < num_developer) {
+        int dev1 = pos[i1][j1], man2 = pos[i2][j2] - num_developer;
+        if (developers[dev1].company == managers[man2].company) {
+            ret += bonus_potential(developers[dev1], managers[man2]);
+        }
+    } else if (pos[i2][j2] < num_developer) {
+        int man1 = pos[i1][j1] - num_developer, dev2 = pos[i2][j2];
+        if (managers[man1].company == developers[dev2].company) {
+            ret += bonus_potential(developers[dev2], managers[man1]);
+        }
+    } else {
+        int man1 = pos[i1][j1] - num_developer, man2 = pos[i2][j2] - num_developer;
+        if (managers[man1].company == managers[man2].company) {
+            ret += bonus_potential(managers[man1], managers[man2]);
+        }
+    }
+    return ret;
+}
+
+int calc_score(vector<vector<int>>& pos) {
+    int score = 0;
+    REP(i1, H) {
+        REP(j1, W) {
+            if (pos[i1][j1] < 0) continue;
+            REP(k, 2) {
+                int i2 = i1 + DIRECTIONS[k].first;
+                int j2 = j1 + DIRECTIONS[k].second;
+                if (i2 >= H || j2 >= W || pos[i2][j2] < 0) continue;
+                int tmp = total_potential(pos, i1, j1, i2, j2);
+                score += total_potential(pos, i1, j1, i2, j2);
+            }
+        }
+    }
+    return score;
+}
+
 void read_input() {
     cin >> W >> H;
     board.resize(H);
-    pos.assign(H, vector<int>(W, -3));
+    ans_pos.assign(H, vector<int>(W, -3));
     for (int i = 0; i < H; i++) cin >> board[i];
     int avail_developer_cnt = 0;
     int avail_manager_cnt = 0;
     for (int i = 0; i < H; i++) {
         for (int j = 0; j < W; j++) {
             if (board[i][j] == '_') {
-                pos[i][j] = -1;
+                ans_pos[i][j] = -1;
                 avail_developer_cnt++;
             }
             if (board[i][j] == 'M') {
-                pos[i][j] = -2;
+                ans_pos[i][j] = -2;
                 avail_manager_cnt++;
             }
         }
@@ -197,5 +252,6 @@ int main() {
     read_input();
 
     initialize_by_valid();
+    cerr << "initial score: " << calc_score(ans_pos) << endl;
     print_solution();
 }
